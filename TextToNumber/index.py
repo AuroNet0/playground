d15035377ef5a7f10046e49c0457182a76a53d01
Auro Neto <128399450+AuroNet0@users.noreply.github.com>
2025-12-04 23:34:58 +0000

    Ficou bem legal até, estou orgulhoso pois não mexia com python a um bom tempo.

--- TextToNumber/index.py
+++ TextToNumber/index.py
@@ -1,42 +1,49 @@
 numberByUnits = {
-    "zero" : 0,
-    "um": 1,
-    "dois": 2,
-    "tres": 3,
-    "quatro": 4,
-    "cinco": 5,
-    "seis": 6,
-    "sete": 7,
-    "oito": 8,
-    "nove": 9,
+    "zero": 0, "um": 1, "dois": 2, "tres": 3, "quatro": 4, 
+    "cinco": 5, "seis": 6, "sete": 7, "oito": 8, "nove": 9,
 }
 
-numbersByDozens = {
-    "dez": 10,
-    "vinte": 20,
-    "trinta": 30,
-    "quarenta": 40,
-    "cinquenta": 50,
-    "sessenta": 60,
-    "setenta": 70,
-    "oitenta": 80,
-    "noventa": 90,
+numbersByDozensAndAssistants = {
+    "e": 0, "dez": 10, "onze": 11, "doze": 12, "treze": 13, "quatorze": 14, 
+    "quinze": 15, "dezesseis": 16, "dezessete": 17, "dezoito": 18, 
+    "dezenove": 19, "vinte": 20, "trinta": 30, "quarenta": 40, 
+    "cinquenta": 50, "sessenta": 60, "setenta": 70, "oitenta": 80, "noventa": 90,
 }
 
 numberByHundreds = {
-    "cem": 100,
-    "duzentos": 200,
-    "trezentos": 300,
-    "quatrocentos": 400,
-    "quinhentos": 500,
-    "seiscentos": 600,
-    "setecentos": 700,
-    "oitocentos": 800,
-    "novecentos": 900,
+    "cento": 100, "cem": 100, "duzentos": 200, "trezentos": 300, 
+    "quatrocentos": 400, "quinhentos": 500, "seiscentos": 600, 
+    "setecentos": 700, "oitocentos": 800, "novecentos": 900,
 }
 
-inputTextValue = str(input("Digite um valor no formato escrito: "));
+full_number_map = {
+    **numberByUnits,
+    **numbersByDozensAndAssistants,
+    **numberByHundreds
+}
 
-for key,value in numberByUnits.items():
-    if (key == inputTextValue ):
-        print("O valor digitado é:",value, ".");
+
+def convertTextToNumber ():
+    inputTextValue = str(input("Digite um valor no formato escrito: "));
+
+    if(inputTextValue.count(' ') > 0):
+        result = findNumberWithSpace(inputTextValue)
+        print("O valor digitado é: ", result)
+    else:
+        result = findNumberWithoutSpace(inputTextValue)
+        print("O valor digitado é: ", result)
+
+def findNumberWithoutSpace (inputTextValue):
+    for key,value in full_number_map.items():
+        if (key == inputTextValue ):
+            return value
+
+def findNumberWithSpace (inputTextValue):
+    textNumbersList = inputTextValue.split(' ')
+    resultSum = 0
+    for number in textNumbersList:
+        searchResult = findNumberWithoutSpace(number)
+        resultSum += searchResult
+    return resultSum
+
+convertTextToNumber()
